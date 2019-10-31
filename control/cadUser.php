5f3d0dfe92f6727826d660adc98d77ece4196cd0
lucasgsilva190 <lucasgsilva190@gmail.com>
2019-10-30 22:32:17 -0300

    att

--- control/cadUser.php
+++ control/cadUser.php
@@ -1,20 +1,21 @@
 <?php   
 
-$nome = [$_POST(nome)];
-$email = [$_POST(email)];
-$cpf= [$_POST(cpf)];
-$senha= [$_POST(senha)];
-$endereco= [$_POST(endereco)];
-$uf= [$_POST(uf)];
-$cidade= [$_POST(cidade)];
-$dataN= [$_POST(dataN)];
-$prof= [$_POST(prof)];
+include_once '../dao/UsuarioDao.php';
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$cpf= $_POST['cpf'];
+$senha= $_POST['senha'];
+$endereco= $_POST['endereco'];
+$uf= $_POST['uf'];
+$cidade= $_POST['cidade'];
+$dataN= $_POST['dataN'];
+$prof= $_POST['prof'];
 
 
-$userDao = new UsuarioDao;
+$userDao = new UsuarioDao();
 
 $userDao->cadastrar($nome,$email,$cpf,$senha,$endereco,$uf,$cidade,$dataN,$prof,0);
-
 
 
 ?>
--- dao/UsuarioDao.php
+++ dao/UsuarioDao.php
@@ -24,19 +24,12 @@ class UsuarioDao{
 
         $con = Conexao::getInstance();
 
-        $sql = 'INSERT INTO usuario (null,Nome,Email,CPF,Senha,Endereco,UF,Cidade,DataDeNascimento,Profissao,Flag) values ('.$nome.''.$email.''.$cpf.''.$senha.''.
-        $endereco.''.$uf.''.$cidade.''.$dataN.''.$prof.','.$flag.');';
+        $sql = "INSERT INTO usuario (Nome,Email,CPF,Senha,Endereco,Estado,Cidade,DataDeNascimento,Profissao,Flag) values ('.$nome.','.$email.','.$cpf.','.$senha.','.$endereco.','.$uf.','.$cidade.','.$dataN.','.$prof.','.$flag.');'";
 
         $stmt = $con->prepare($sql);
        
         $stmt->execute();
-        
-        if($stmt->rowCount() == 1){
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        return null;
 
     }
-
 }
 ?>
