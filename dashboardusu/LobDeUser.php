ce532cf8e86e1c38b38c43bf563c5aa6ba9e0cb3
Lucas <lucasgsilva190@gmail.com>
2020-03-25 16:30:24 -0300

    att

--- dashboardusu/LobDeUser.php
+++ dashboardusu/LobDeUser.php
@@ -93,7 +93,7 @@ exit();
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                  <a href="perfilUser.php" class="btn btn-default btn-flat">Perfil</a>
                 </div>
                 <div class="pull-right">
                   <a href="../control/logout.php" class="btn btn-default btn-flat">Sair</a>
