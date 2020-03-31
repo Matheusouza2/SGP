b8c8ad0dceccb414b2159ba85829351583469ca8
Lucas <lucasgsilva190@gmail.com>
2020-03-31 17:08:07 -0300

    att Seletor de Cursos Cadastro

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if(!isset($_SESSION['nome']) && !isset($_SESSION['email']))
-    header('location: view/telaLogin.php');
-else
-	header('location: dashboardusu/LobDeUser.php');
-
-?>
