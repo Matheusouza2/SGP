9521f3f918bf54691dd7a53f900149b1ca708479
Matheusouza2 <matheusouza1997@gmail.com>
2020-09-15 09:38:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['usuarioLogado']))
-    header('location: /sgp/view/telaLogin.php');
+    header('location: sgp/view/telaLogin.php');
 else
-	header('location: /sgp/dashboardusu/src/html/index.php');
+	header('location: sgp/dashboardusu/src/html/index.php');
 ?>
