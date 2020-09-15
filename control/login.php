5ce9514262b634daadf9b55664bf37c773f39385
11039083404 <11039083404@est204378.codevasf.gov.br>
2020-09-15 11:57:21 -0300

    Heroku 2

--- control/login.php
+++ control/login.php
@@ -1,15 +1,23 @@
 <?php
+echo "<script> console.log ('Inicio do arquivo'); </script>";
 include_once '..\dao\UsuarioDao.php';
+echo "<script> console.log ('Inclusão do usuarioDAO'); </script>";
+
 
 session_start();
+echo "<script> console.log ('Sessao Iniciada'); </script>";
 $email = addslashes($_POST['email']);
 $senha = addslashes($_POST['senha']);
 
+echo "<script> console.log ('Infos capturadas-->'".$email."); </script>";
+
 $usuDao = new UsuarioDao();
+
+echo "<script> console.log ('Classe usuDao instanciada'); </script>";
 
 $usuLogado = $usuDao->login($email, $senha);
 
-
+echo "<script> console.log ('Tentativa de login e retorno'".$usuLogado."); </script>";
 if (! empty($usuLogado)) {
 	
    // $usuDao->buscar($emal); ver depois nao apagar
