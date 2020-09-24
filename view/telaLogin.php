b1d0a9b71fb23407425490901622a7bc875e8f89
lucasgsilva190 <lucasgsilva190@gmail.com>
2020-09-24 13:47:53 -0300

    att

--- view/telaLogin.php
+++ view/telaLogin.php
@@ -24,34 +24,34 @@ if (isset($_SESSION['nome']))
 
 <body>
 
-<div class="container-fluid">
-	<section class="form-section">
-		<img src="../assets/img/logosvgbranco.svg" width="160">
+	<div class="container-fluid">
+		<section class="form-section">
+			<img src="../assets/img/logosvgbranco.svg" width="160">
 
 
-		<form action="../control/login.php" method="POST" class="needs-validation" enctype="application/x-www-form-urlencoded" novalidate>
-			<div class="form">
-				<div style="margin: 7px;" class="col-lg-15 mb-10">
-					<label for="validationCustom01">Email</label> <input type="email" class="form-control" id="email" placeholder="Email" required name="email">
-					<div class="valid-feedback">Tudo certo!</div>
-					<div class="invalid-feedback">Email Invalido!</div>
+			<form action="../control/login.php" method="POST" class="needs-validation" enctype="application/x-www-form-urlencoded" novalidate>
+				<div class="form">
+					<div class="col-lg-15 mb-10">
+						<label for="validationCustom01">Email</label> <input type="email" class="form-control input-block nth-child" id="email" placeholder="Email" required name="email">
+						<div class="valid-feedback">Tudo certo!</div>
+						<div class="invalid-feedback">Email Invalido!</div>
+					</div>
+
+					<div class="col-lg-15 mb-10">
+						<label for="validationCustom02">Senha</label> <input type="password" class="form-control input-block nth-child" id="validationCustom02" placeholder="Senha" required name="senha">
+
+						<div class="invalid-feedback">Inserir Senha!</div>
+					</div>
+				</div>
+				<div style="text-align: center; padding-top: 2%;">
+					<button class="btn btn-success" onclick="validar()" type="submit">Login</button>
 				</div>
 
-				<div style="margin: 7px;" class="col-lg-15 mb-10">
-					<label for="validationCustom02">Senha</label> <input type="password" class="form-control" id="validationCustom02" placeholder="Senha" required name="senha">
-
-					<div class="invalid-feedback">Inserir Senha!</div>
+				<div style="text-align: center;">
+					<a>Ainda não tem conta? <a href="cadastroUsuario.php" style="color: green;">Cadastre-se</a></a>
 				</div>
-			</div>
-			<div style="text-align: center; padding-top: 2%;">
-				<button class="btn btn-success" onclick="validar()" type="submit">Login</button>
-			</div>
-
-			<div style="text-align: center;">
-				<a>Ainda não tem conta? <a href="cadastroUsuario.php" style="color: green;">Cadastre-se</a></a>
-			</div>
-		</form>
-	</section>
+			</form>
+		</section>
 	</div>
 
 
