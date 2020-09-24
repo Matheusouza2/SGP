9427dc30379e380cf93a29983e7c5ec5b6a68f98
lucasgsilva190 <lucasgsilva190@gmail.com>
2020-09-24 14:16:59 -0300

    animation index

--- view/telaLogin.php
+++ view/telaLogin.php
@@ -31,20 +31,21 @@ if (isset($_SESSION['nome']))
 
 			<form action="../control/login.php" method="POST" class="needs-validation" enctype="application/x-www-form-urlencoded" novalidate>
 				<div class="form">
-					<div class="col-lg-15 mb-10">
-						<label for="validationCustom01">Email</label> <input type="email" class="form-control input-block nth-child" id="email" placeholder="Email" required name="email">
+				
+				<div style="margin: 1rem;" class="col-lg-15 mb-10">
+						<label for="validationCustom01">Email</label> <input type="email" class="texto form-control " id="email" placeholder="Email" required name="email">
 						<div class="valid-feedback">Tudo certo!</div>
 						<div class="invalid-feedback">Email Invalido!</div>
 					</div>
 
-					<div class="col-lg-15 mb-10">
-						<label for="validationCustom02">Senha</label> <input type="password" class="form-control input-block nth-child" id="validationCustom02" placeholder="Senha" required name="senha">
+					<div style="margin: 1rem;" class="col-lg-15 mb-10">
+						<label for="validationCustom02">Senha</label> <input type="password" class="texto2 form-control " id="validationCustom02" placeholder="Senha" required name="senha">
 
 						<div class="invalid-feedback">Inserir Senha!</div>
 					</div>
 				</div>
-				<div style="text-align: center; padding-top: 2%;">
-					<button class="btn btn-success" onclick="validar()" type="submit">Login</button>
+				<div style="text-align: center; ">
+					<button class="btn btn-success movebutton" onclick="validar()" type="submit">Login</button>
 				</div>
 
 				<div style="text-align: center;">
