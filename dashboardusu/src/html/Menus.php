55c708013bbcb4a782c1962b7a1ec4e6cbe603ee
Lucas Gondim <lucasgsilva190@gmail.com>
2020-11-21 14:14:30 -0300

    tirando icones e colocando legenda

--- dashboardusu/src/html/Menus.php
+++ dashboardusu/src/html/Menus.php
@@ -62,55 +62,17 @@
             <!-- toggle and nav items -->
             <!-- ============================================================== -->
             <ul class="navbar-nav float-left mr-auto ml-3 pl-1">
+
+            <h3> Sistema de Gerenciamento de Permutas</h3>
                 <!-- Notification -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle pl-md-3 position-relative" href="javascript:void(0)" id="bell" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span><i data-feather="bell" class="svg-icon"></i></span>
-                        <span class="badge badge-primary notify-no rounded-circle">3</span>
-                    </a>
+                    
                     <div class="dropdown-menu dropdown-menu-left mailbox animated bounceInDown">
                         <ul class="list-style-none">
                             <li>
-                                <div class="message-center notifications position-relative">
-                                    <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <div class="btn btn-danger rounded-circle btn-circle"><i data-feather="airplay" class="text-white"></i></div>
-                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                            <h6 class="message-title mb-0 mt-1">Usuário 1</h6>
-                                            <span class="font-12 text-nowrap d-block text-muted">Permuta recusada</span>
-                                            <span class="font-12 text-nowrap d-block text-muted">9:30 AM</span>
-                                        </div>
-                                    </a>
-                                    <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <span class="btn btn-success text-white rounded-circle btn-circle"><i data-feather="calendar" class="text-white"></i></span>
-                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                            <h6 class="message-title mb-0 mt-1">Usuário 2</h6>
-                                            <span class="font-12 text-nowrap d-block text-muted text-truncate">Just
-                                                Permuta Aceita</span>
-                                            <span class="font-12 text-nowrap d-block text-muted">9:10 AM</span>
-                                        </div>
-                                    </a>
-                                    <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <span class="btn btn-info rounded-circle btn-circle"><i data-feather="settings" class="text-white"></i></span>
-                                        <div class="w-75 d-inline-block v-middle pl-2">
-                                            <h6 class="message-title mb-0 mt-1">Cadastro</h6>
-                                            <span class="font-12 text-nowrap d-block text-muted text-truncate">
-                                                Agora você pode inserir Foto</span>
-                                            <span class="font-12 text-nowrap d-block text-muted">9:08 AM</span>
-                                        </div>
-                                    </a>
-                                    <!-- Message -->
-
-                                </div>
+                            
                             </li>
-                            <li>
-                                <a class="nav-link pt-3 text-center text-dark" href="javascript:void(0);">
-                                    <strong>Ver todas as notificações</strong>
-                                    <i class="fa fa-angle-right"></i>
-                                </a>
-                            </li>
+                          
                         </ul>
                     </div>
                 </li>
@@ -119,15 +81,8 @@
                 <!-- create new -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="settings" class="svg-icon"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Ação 1</a>
-                        <a class="dropdown-item" href="#">Ação 2</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Ação 3</a>
-                    </div>
+               
+                    
                 </li>
                 <!-- <li class="nav-item d-none d-md-block">
                             <a class="nav-link" href="javascript:void(0)">
